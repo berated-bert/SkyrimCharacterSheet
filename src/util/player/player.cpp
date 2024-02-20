50ed8fe3e7cec90eb89ff5cd30f05914f1f42c97
berated-bert <91331516+berated-bert@users.noreply.github.com>
2024-02-20 10:27:30 +0800

    Update player.cpp to fix blade and blunt armor calc

--- src/util/player/player.cpp
+++ src/util/player/player.cpp
@@ -175,7 +175,11 @@ namespace util {
         }
 
         if (mod::mod_manager::get_singleton()->get_blade_and_blunt()) {
-            auto resistance = a_armor_rating * game_settings->get_armor_scaling_factor() + 0.03f * 100 * a_pieces_worn;
+
+            // If armor rating <= 500, formula is DR = 0.15 * Armor rating
+
+            
+            auto resistance = a_armor_rating * game_settings->get_armor_scaling_factor();
 
             return mod::blade_and_blunt::calculate_armor_damage_resistance(resistance);
         }
